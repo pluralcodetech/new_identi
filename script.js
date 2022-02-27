e6d48bb4dc80763934a31f840373eac954bb26e9
cycodedconcept <cycodedconcept@gmail.com>
2022-02-27 13:55:53 +0100

    Dispute Resolution form completed

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 let input = document.querySelector('#phone');
+let input2 = document.querySelector('#phone2');
 let iti = window.intlTelInput(input, {
+    utilsScript: 'utils.js'
+})
+
+let iti2 = window.intlTelInput(input2, {
     utilsScript: 'utils.js'
 })
